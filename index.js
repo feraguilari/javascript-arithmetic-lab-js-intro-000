82b11939ab99024d518837ea2bc676c0660bdd19
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-04 05:40:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ function dec(n){
 function makeInt(n){
   return parseInt(n, 10)
 }
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+function preserveDecimal(n){
+  return parseFloat("Hello")
+}
